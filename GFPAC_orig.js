9f6063e3a1da1544caebf93b1d6c2272fce1fc00
eryitrika <eryitrika@users.noreply.github.com>
2021-04-04 01:48:44 +0900

    Add files via upload

---
+++ GFPAC_orig.js
@@ -0,0 +1,11 @@
+function FindProxyForURL(url, host) {
+    // 한섭 고화질 프록시 서버
+    if (dnsDomainIs(host, "sn-list.girlfrontline.co.kr")) {
+        return "PROXY klanet.iptime.org:9000";
+    }
+    // 중섭 고화질 프록시 서버
+    else if (dnsDomainIs(host, "gf-cn.oss-cn-beijing.aliyuncs.com")) {
+        return "PROXY klanet.iptime.org:9000";
+    }
+    return "DIRECT";
+}
