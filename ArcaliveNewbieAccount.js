f3eeae5e175f1325f8f7c82a173044aeb08c24ef
c2MzQ7guYryb <eryitrika@users.noreply.github.com>
2024-02-06 02:07:29 +0900

    Update ArcaliveNewbieAccount.js

--- ArcaliveNewbieAccount.js
+++ ArcaliveNewbieAccount.js
@@ -7,8 +7,8 @@
     // @grant        GM_xmlhttpRequest
     // @downloadURL  https://greasyfork.org/ko/scripts/486695-%EC%95%84%EC%B9%B4%EB%9D%BC%EC%9D%B4%EB%B8%8C-%EA%B9%A1%EA%B3%84-%ED%99%95%EC%9D%B8-%EC%88%98%EC%A0%95%EB%B3%B8
     // @updateURL    https://update.greasyfork.org/scripts/486695/%EC%95%84%EC%B9%B4%EB%9D%BC%EC%9D%B4%EB%B8%8C%20%EA%B9%A1%EA%B3%84%20%ED%99%95%EC%9D%B8%20%EC%88%98%EC%A0%95%EB%B3%B8.user.js
-    // @referenceURL https://arca.live/b/arknights/98344291
     // ==/UserScript==
+    // referenceURL  https://arca.live/b/arknights/98344291
      
     const links = document.querySelectorAll('.info-row .user-info a');
      
