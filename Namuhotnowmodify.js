771f0ce5eb592eb3174dafb156866f4dec3b20a9
c2MzQ7guYryb <eryitrika@users.noreply.github.com>
2024-02-16 10:10:30 +0900

    Update Namuhotnowmodify.js

--- Namuhotnowmodify.js
+++ Namuhotnowmodify.js
@@ -2,7 +2,7 @@
     // @name         Namu Hot Now
     // @name:ko      나무위키 실검 알려주는 스크립트
     // @namespace    https://arca.live/b/namuhotnow
-    // @version      0.5.7
+    // @version      0.6.2
     // @description  이게 왜 실검?
     // @author       KEMOMIMI
     // @match        https://namu.wiki/*
@@ -13,8 +13,6 @@
     // @connect      arca.live
     // @icon         https://www.google.com/s2/favicons?sz=64&domain=namu.wiki
     // @grant        GM_xmlhttpRequest
-    // @downloadURL  https://raw.githubusercontent.com/eryitrika/storage/master/Namuhotnowmodify.js
-    // @updateURL    https://raw.githubusercontent.com/eryitrika/storage/master/Namuhotnowmodify.js
     // ==/UserScript==
      
     function findLinkByPartialMatch(pairs, searchString) {
@@ -32,13 +30,13 @@
         spans.forEach(function(span) {
             spansContent.push(span.textContent);
         });
-        return spansContent;
+        return spansContent.join('').trim();
     }
      
     const targetURL = 'https://arca.live/b/namuhotnow';
     var linkElements = [];
     var pairs = [];
-    var previousSpansContent = getSpansContent();
+    var previousSpansContent = "";
     var storedElements = [];
      
     function removeLinkElements() {
@@ -50,36 +48,51 @@
     }
      
     function checkMobileHotkeword(){
-        var chkMobileValId = setInterval(function() {
-            var parentElement = document.querySelector('#app form div > div').parentNode;
-            var namuHotKeyword = document.querySelectorAll('.namuHotParentClass a');
-            var hrefCnt = 0
-            namuHotKeyword.forEach(function(element) {
-                if(element.getAttribute('href') === '#'){
-                    hrefCnt+=1
-                }
-            });
-            if (hrefCnt <10 && window.getComputedStyle(parentElement).display === "none") {
-                var childCount = parentElement.children.length;
+        var chk = setInterval(function() {
      
-                if (childCount <= 11) {
-                    var namuHotElements = document.querySelectorAll('.namuHotParentClass');
+            const appElement = document.querySelector('#app form div > div');
+            if (!appElement) {
+                var whyHotElements = document.querySelectorAll('.whyHot');
+                whyHotElements.forEach(function(element) {
      
-                    namuHotElements.forEach(function(element) {
-                        element.remove();
+                    element.remove();
+                });
+                const elementsWithParentClass = document.querySelectorAll('.namuHotParentClass');
+                elementsWithParentClass.forEach(parentElement => {
+                    const childAElement = parentElement.querySelector('a');
+                    if (childAElement) {
+                        parentElement.parentNode.insertBefore(childAElement, parentElement.nextSibling);
+                        parentElement.remove();
+                    }
+                });
+            }else{
+                if (document.querySelector('#app form div>div').textContent.trim() === '실시간 검색어') {
+                    const elementsWithParentClass = document.querySelectorAll('.namuHotParentClass');
+                    let count = 0;
+                    elementsWithParentClass.forEach(parentElement => {
+                        const childAnchorElements = parentElement.querySelectorAll('a');
+                        childAnchorElements.forEach(anchorElement => {
+                            if (anchorElement.getAttribute('href') === '#') {
+                                count++;
+                            }
+                        });
                     });
-                    clearInterval(chkMobileValId);
-                    var interNamuMobile = setInterval(function() {
-                        var parentElement = document.querySelector('#app form div > div').parentNode;
-                        var childCount = parentElement.children.length;
-                        if (childCount >= 10) {
-                            clearInterval(interNamuMobile);
-                            refreshLink(2);
-                        }
-                    }, 100);
+     
+                    if (count == 0) {
+                        const elementsWithParentClass = document.querySelectorAll('.namuHotParentClass');
+                        elementsWithParentClass.forEach(function(element) {
+                            element.remove();
+                        });
+                    }
+                    if (elementsWithParentClass.length == 0) {
+                        clearInterval(chk);
+                        refreshLink(2);
+                    }
                 }
             }
         }, 100);
+     
+     
     }
     function refreshLink(type) {
         GM_xmlhttpRequest({
@@ -120,7 +133,6 @@
                     }
                 });
                 if(type == 0){
-                    previousSpansContent = getSpansContent();
                     var spans = document.querySelectorAll('#app span');
                     spans.forEach(function (span) {
                         if (span.textContent.includes('실시간 검색어')) {
@@ -157,7 +169,7 @@
                             var newSpanHTML = `
     <div style="padding:.15rem .5rem .15rem 0; user-select: auto;">
     <span class="leaf-info float-right" title="${aLinkElement.getAttribute('title')} 왜 실검?" style="margin:0; user-select: auto;"><time style="user-select: auto;"><a href="${resultLink}" target="_blank" style="font-size: 1em; padding-Right: 0; user-select: auto;">이유보기</a></time></span>
-    <a href="//namu.wiki/Go?q=${aLinkElement.getAttribute('title')}" target="_blank" title="${aLinkElement.getAttribute('title')}" style="user-select: auto;">${aLinkElement.getAttribute('title')}</a>
+    <a href="//namu.wiki/Go?q=${aLinkElement.getAttribute('title')}" target="_blank" title="${aLinkElement.getAttribute('title')}" style="padding:.15rem 1.5rem .15rem 0; user-select: auto;">${aLinkElement.getAttribute('title')}</a>
     </div>
     `;
                             aLinkElement.insertAdjacentHTML('beforebegin', newSpanHTML);
@@ -166,39 +178,42 @@
                         }
                     });
                 }else if(type == 2){
-                    var realtimeMobile = document.querySelector('#app form div>div');
-                    var mobileParentDiv = realtimeMobile.parentElement;
-                    var mobileList = mobileParentDiv.querySelectorAll('a');
+                    var namuHotParentClass = document.querySelectorAll('.namuHotParentClass');
+                    if (!namuHotParentClass[0]) {
+                        var realtimeMobile = document.querySelector('#app form div>div');
+                        var mobileParentDiv = realtimeMobile.parentElement;
+                        var mobileList = mobileParentDiv.querySelectorAll('a');
      
-                    mobileList.forEach(function(element) {
-                        var resultLink = findLinkByPartialMatch(pairs, element.innerText.trim());
-                        var newParent = document.createElement('span');
-                        newParent.classList.add('namuHotParentClass');
+                        mobileList.forEach(function(element) {
+                            var resultLink = findLinkByPartialMatch(pairs, element.innerText.trim());
+                            var newParent = document.createElement('span');
+                            newParent.classList.add('namuHotParentClass');
      
+                            if (resultLink != null){
      
-                        if (resultLink != null){
-                            var linkElement = document.createElement('a');
-                            linkElement.href = 'https://arca.live' + resultLink;
-                            linkElement.textContent = '이유보기';
-                            linkElement.width = '20px'
-                            linkElement.target="_blank"
-                            linkElement.classList.add('whyHot');
-                            element.style.width = "70%";
-                            element.classList.add('mobileHotKeyword');
-                            var beforePseudoElement = window.getComputedStyle(element, ':before');
-                            element.parentNode.insertBefore(newParent, element);
-                            newParent.appendChild(element);
-                            newParent.appendChild(linkElement);
-                            newParent.style.display = 'flex';
-                            linkElements.push(linkElement);
-                        }else{
-                            element.parentNode.insertBefore(newParent, element);
-                            newParent.appendChild(element);
-                            element.style.width = "100%";
-                            newParent.style.display = 'flex';
-                        }
-                    });
-                    checkMobileHotkeword();
+                                var linkElement = document.createElement('a');
+                                linkElement.href = 'https://arca.live' + resultLink;
+                                linkElement.textContent = '이유보기';
+                                linkElement.width = '20px'
+                                linkElement.target="_blank"
+                                linkElement.classList.add('whyHot');
+                                element.style.width = "70%";
+     
+                                var beforePseudoElement = window.getComputedStyle(element, ':before');
+                                element.parentNode.insertBefore(newParent, element);
+                                newParent.appendChild(element);
+                                newParent.appendChild(linkElement);
+                                newParent.style.display = 'flex';
+                                linkElements.push(linkElement);
+                            }else{
+                                element.parentNode.insertBefore(newParent, element);
+                                newParent.appendChild(element);
+                                element.style.width = "100%";
+                                newParent.style.display = 'flex';
+                            }
+                        });
+                        checkMobileHotkeword();
+                    }
                 }
      
             }
@@ -206,13 +221,15 @@
     }
      
      
-    function spansContentChanged(arr1, arr2) {
-        return arr1.join() !== arr2.join();
-    }
+    function hasRealTimeKeyword() {
+        const spans = document.querySelectorAll('#app div > div > span');
      
-    function hasAsideTag() {
-        var asideElement = document.querySelector('aside');
-        return asideElement !== null;
+        for (const span of spans) {
+            if (span.textContent.trim() === '실시간 검색어') {
+                return true;
+            }
+        }
+        return false;
     }
      
      
@@ -264,23 +281,27 @@
     }
      
     if (window.location.href.includes('namu.wiki')) {
-        refreshLink(0);
-        setInterval(function() {
-            var currentSpansContent = getSpansContent();
-            if (spansContentChanged(previousSpansContent, currentSpansContent)) {
-                removeLinkElements();
-                refreshLink(0);
-            }
-     
-        }, 1000);
-        var interNamuMobile = setInterval(function() {
-            var firstLinkLista = document.querySelector('#app form div>div');
-            if (firstLinkLista.textContent.trim() === '실시간 검색어') {
-                clearInterval(interNamuMobile);
-                appendStyle();
-                refreshLink(2);
-            }
-        }, 50);
+        if(hasRealTimeKeyword()){
+            setInterval(function() {
+                var content = getSpansContent();
+                if (content.length > 0 && previousSpansContent !== getSpansContent()) {
+                    previousSpansContent = getSpansContent();
+                    removeLinkElements();
+                    refreshLink(0);
+                }
+            }, 100);
+        }else{
+            var interNamuMobile = setInterval(function() {
+                var firstLinkLista = document.querySelector('#app form div > div');
+                if (firstLinkLista) {
+                    if (firstLinkLista.textContent.trim() === '실시간 검색어') {
+                        clearInterval(interNamuMobile);
+                        appendStyle();
+                        refreshLink(2);
+                    }
+                }
+            }, 50);
+        }
     }
      
      
